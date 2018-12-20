cd776d40fbf3d6f2c3f5bcf9de01338e2a4fbcf9
Berend Kalberg <berend.kalberg@gmail.com>
2018-12-20 13:58:44 +0100

    Navbar update

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar sticky-top navbar-expand-md navbar-dark bg-dark">
         <div class="container">
             <a class="navbar-brand" href="{{ url('/') }}">
-                {{ config('app.name', 'Laravel') }}
+                Berend Kalberg
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                 <span class="navbar-toggler-icon"></span>
@@ -10,9 +10,6 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <!-- Left Side Of Navbar -->
                 <ul class="navbar-nav mr-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="/">Startpagina</a>
-                    </li>
                     <li class="nav-item">
                         <a class="nav-link" href="/about">Over mij</a>
                     </li>
