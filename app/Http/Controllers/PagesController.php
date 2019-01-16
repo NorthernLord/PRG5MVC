b73f6b1d33479bc7f3a3a202488c0249a1826642
Berend Kalberg <berend.kalberg@gmail.com>
2019-01-16 10:36:02 +0100

    Added UX design to array

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -13,7 +13,7 @@ class PagesController extends Controller
     public function services() {
         $data = array(
             'title' => 'Services',
-            'services' => ['Web Design', 'Programming']
+            'services' => ['Web Design','UX Design' ,'Programming']
         );
         return view('pages.services')->with($data);
     }
